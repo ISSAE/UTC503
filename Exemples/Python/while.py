037ecfa9c4e1b296ce0d8509ef69fad2869de062
pascalfares <pascal.fares@isae.edu.lb>
2019-10-14 12:02:29 +0300

    if while bloc python

---
+++ Exemples/Python/while.py
@@ -0,0 +1,22 @@
+x=0
+a=0
+print(x)
+print(a)
+while (x<10):
+    x=x+1
+    print("Dans la boucle ",x,end=": ")
+    a=a+1
+print("\nA la sortie de la boucle x>=10",x)
+print("Que vaut a? ", a)
+print("\n===Nouvelle execution===\n")
+x=0
+a=0
+print(x)
+print(a)
+while (x<10):
+    x=x+1
+    print("Dans la boucle ",x,end=": ")
+a=a+1
+print("\nA la sortie de la boucle x>=10",x)
+print("Que vaut a? ", a)
+
