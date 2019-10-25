72870ac9dfe4e424bbc09a0c7712b7f2fe9c606e
pascalfares <pascal.fares@isae.edu.lb>
2019-10-25 11:57:49 +0300

    Fonctions imbriquées

---
+++ Seances/Python/Classes/001/Encapsulation/encapsul.py
@@ -0,0 +1,19 @@
+class X:
+    def __m(self):
+        return ('dans __m')
+
+'''$ python -i encapsul.py 
+>>> x=X()
+>>> dir(x)
+['_X__m', '__doc__', '__module__']
+>>> x.m()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: X instance has no attribute 'm'
+>>> x.__m()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: X instance has no attribute '__m'
+>>> x._X__m()
+'dans __m'
+>>> '''
