795480073cc9750f4e1779358a168d49d1513b6e
pascalfares <pascal.fares@isae.edu.lb>
2019-12-02 06:26:16 +0200

    Exemple fonctionnel zero de fonction

---
+++ Seances/Python/Classes/multiple/mltiple.py
@@ -0,0 +1,29 @@
+class Base:
+    def m(self,x=0):
+        return f"Dans Base {x}"
+
+class A(Base):
+    def m(self,x):
+        return f"Dans A {x}"
+
+class B(Base):
+    def m(self,x):
+       return f"Dans B {x}"
+class C(A,B):
+    pass
+class D(A,B):
+    def m(self,x):
+        return Base.m(self, x)
+
+base=Base()
+a=A()
+b=B()
+c=C()
+d=D()
+print(base.m())
+print(a.m(1))
+print(b.m(2))
+print(c.m(3))
+print(d.m(4))
+
+
