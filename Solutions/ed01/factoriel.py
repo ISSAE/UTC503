401abefec9f44ef2dc267d639acac298ab00bc7b
pascalfares <pascal.fares@isae.edu.lb>
2019-12-14 15:36:12 +0200

    Dérécursifier première étape accumulation def _factTer(i,acc,n):     # Invariant acc=i!     if (i==n): return acc #Condition de fin     else: return _factTer(i+1,acc*(i+1),n) #Condition de répétition def factTer(n):     # 0!=1     # i=0, acc=1     return _factTer(0,1,n)

--- Solutions/ed01/factoriel.py
+++ Solutions/ed01/factoriel.py
@@ -12,15 +12,27 @@ def _factNeg(n):
 def fact(n):
     if (n<0): return _factNeg(n)
     else: return _fact(n)
-
-# d'autres solutions itérative cas n>=0
+# Factorielle récursive terminale (par accumulation)
+def _factTer(i,acc,n):
+    # Invariant acc=i!
+    if (i==n): return acc #Condition de fin
+    else: return _factTer(i+1,acc*(i+1),n) #Condition de répétition
+def factTer(n):
+    # 0!=1
+    # i=0, acc=1
+    return _factTer(0,1,n)
+# d'autres solutions itérative cas n>=0 issue de la récursivité
 def factWhile(n):
     #0!=1
     F=1
     i=0
     while (i<n):
+        # Dans la boucle 
+        # provient de else: return _factTer(i+1,acc*(i+1),n)
         i+=1
         F=F*i
+    # sortie de boucle i==n 
+    # provient de if (i==n): return acc
     return F
 
 
