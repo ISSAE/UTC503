9d9ca8f77fac35753ebf8ba91431866cc4402769
Pascal Fares WSL_U1804 <pascal.fares@isae.edu.lb>
2019-12-23 14:27:45 +0200

    fractal avec couleur

--- Exemples/Python/Turtlefractal/fractal.py
+++ Exemples/Python/Turtlefractal/fractal.py
@@ -1,9 +1,14 @@
-import turtle
+import turtle,random
 
 
+_color=('black', 'red', 'blue', 'yellow', 'purple', 'cyan', 'Burlywood', 'Aquamarine')
+def getColor():
+    return _color[random.randrange(0,7)]
 
 def _fract1(turt, taille,min):
-    if (taille<=min): turt.forward(taille)
+    if (taille<=min):
+        turt.color(getColor())
+        turt.forward(taille)
     else:
         _fract1(turt, taille/3,min)
         turt.left(60)
@@ -13,11 +18,15 @@ def _fract1(turt, taille,min):
         turt.left(60)
         _fract1(turt, taille/3,min)
 
+
 def fract1(taille, min=5):
     t=turtle.Turtle()
+    t.pensize(3)
+    t.up()
     t.goto(-taille/2,0)
+    t.down()
     _fract1(t,taille,min)
-    t.done()
 
-fract1(500)
+fract1(1000,50)
+input("Une touche pour terminer")
 
