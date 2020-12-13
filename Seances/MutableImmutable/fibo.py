433d776077d001525be595428781ffa801ef7444
Pascal Fares <pascal.fares@cofares.net>
2020-12-13 18:30:33 +0200

    latest

--- Seances/MutableImmutable/fibo.py
+++ Seances/MutableImmutable/fibo.py
@@ -1,5 +1,5 @@
 '''
-procedure de calcul de la suite de fibonati
+procedure de calcul de la suite de fibonacci
 
 f(0) = 0
 f(1) = 1
@@ -7,3 +7,26 @@ f(n) = fn(n-1)+f(n-2)
 '''
 
 # état initial
+l=[0, 1]
+
+def fibo(n, lfibo):
+    '''
+    Calcul des éléments de la suite de fibonacci
+
+    n : le nombre d'éléments nouveaux souhaités
+    lfibo: La liste des élements de la suite
+      IN : les élements initiaux
+      OUT : La suite des n premiers éléments 
+    '''
+    for i in range(n):
+        lfibo.append(lfibo[-1]+lfibo[-2])
+
+
+fibo(3,l)
+print(l)
+fibo(3,l)
+print(l)
+fibo(10,l)
+print(l)
+fibo(100,l)
+print(l)
--- Seances/MutableImmutable/swap.c
+++ Seances/MutableImmutable/swap.c
@@ -4,13 +4,10 @@ int main(int argc, char **argv) {
     int x = 10;
     int y = 20;
     int z;
-
     printf("Avant %d %p %d %p\n", x, &x, y, &y);
-
     //swap
     z=x;
     x=y;
     y=z;
-
     printf("Après %d %p %d %p\n", x, &x, y, &y);
 }
